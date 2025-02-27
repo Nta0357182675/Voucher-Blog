e3b882c26ae960a8e5226ec68b535678c9ce6fcc
tienanh1103 <nguyentienanh11032003@gmail.com>
2025-02-27 18:31:06 +0700

    Thêm slider

--- my-vue-app/src/pages/Home/Home.jsx
+++ my-vue-app/src/pages/Home/Home.jsx
@@ -1,51 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import '../../stylesCss/Home.css';
-
+import React, { useEffect, useState } from "react";
+import "../../stylesCss/Home.css"; // Import file CSS
+import { Swiper, SwiperSlide } from "swiper/react"; // Import Swiper
+import "swiper/css"; // CSS mặc định của Swiper
+import "swiper/css/navigation"; // CSS cho nút điều hướng
+import "swiper/css/pagination"; // CSS cho chấm trượt
+import { Navigation, Pagination } from "swiper/modules"; // Import module Swiper
 
 const Home = () => {
+  // State lưu danh sách game
+  const [listSingleGame, setListSingleGame] = useState([]);
 
-  const [listSingleGame, setListSingleGame] = useState([])
-  // call api dữ liệu game
+  // Gọi API lấy dữ liệu game
   const handleApi = async () => {
     try {
-      const handleRes = await (await fetch('http://localhost:3001/games')).json()
-      setListSingleGame(handleRes)
+      const handleRes = await (await fetch("http://localhost:3001/games")).json();
+      setListSingleGame(handleRes);
     } catch (error) {
-      console.log(`error: ${error}`)
+      console.log(`error: ${error}`);
     }
-  }
+  };
+
   useEffect(() => {
-    handleApi();;
-
-    return () => {}; // Dọn dẹp timeout khi component unmount
+    handleApi();
+    return () => {};
   }, []);
-
 
   return (
     <>
-      <div className='game single-player'>
-        <h3>Game 1 người chơi</h3>
-        <ul>
-          {
-            listSingleGame.map((game) => {
-              return <li key={game.id} className='boxgame'>
-                <div className='imgName'><img src={game.image} alt="" /></div>
-                <div className='info'>
-                  <h4>{game.name}</h4>
-                  <div className='button'>
-                    <button className='play'>Chơi thử</button>
-                    <button className='design'>Thiết kế</button>
+      <div className="game single-player">
+        <h2>Game 1 người chơi</h2>
+
+        {/* Bọc danh sách game trong <ul> */}
+        <ul className="game-list">
+          <Swiper
+            modules={[Navigation, Pagination]} // Kích hoạt điều hướng & chấm trượt
+            spaceBetween={15} // Giảm khoảng cách giữa các game
+            slidesPerView={4} // Hiển thị 4 game trên màn hình lớn
+            navigation // Sử dụng mũi tên mặc định của Swiper
+            // pagination={{ clickable: true }} // Hiển thị chấm trượt
+          >
+            {listSingleGame.map((game) => (
+              <SwiperSlide key={game.id}>
+                <li className="boxgame">
+                  <div className="imgName">
+                    <img src={game.image} alt={game.name} />
                   </div>
-                </div>
-              </li>
-            })
-          }
+                  <div className="info">
+                    <h4>{game.name}</h4>
+                    <div className="button">
+                      <button className="play">Chơi thử</button>
+                      <button className="design">Thiết kế</button>
+                    </div>
+                  </div>
+                </li>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </ul>
       </div>
-      {/* <div className='game multiplayer'></div>
-      <div className='favorite game'></div> */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
